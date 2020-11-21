5a4699ac3f2ea108c85c2811963a063cf4be734b
RedditRook <34390637+RedditRook@users.noreply.github.com>
2020-11-21 23:44:32 +0900

    폭팔후 상황

---
+++ 기말프로젝트/소스코드/after.py
@@ -0,0 +1,20 @@
+from pico2d import *
+from gobj import *
+import gfw_image
+
+class After:
+    after = []
+    def __init__(self, pos, delta):
+        imageName = '/bombafter.png'
+        self.image = gfw_image.load(RES_DIR + imageName)
+        self.pos = pos
+        self.delay=0
+        self.count=0.1
+    def draw(self):
+        self.image.draw(*self.pos,50,50)
+    def update(self):
+        x,y = self.pos
+        self.delay += self.count
+        print(self.delay)
+        if self.delay > 1:
+            After.after.remove(self)

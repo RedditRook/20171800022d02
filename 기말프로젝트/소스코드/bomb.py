5e432b274b085f6febe31a6a2f01de00cba589e6
RedditRook <34390637+RedditRook@users.noreply.github.com>
2020-11-21 16:29:02 +0900

    폭탄 코드 수정

--- 기말프로젝트/소스코드/bomb.py
+++ 기말프로젝트/소스코드/bomb.py
@@ -2,8 +2,8 @@ from pico2d import *
 from gobj import *
 import gfw_image
 
-class Ball:
-    balls = []
+class Bomb:
+    bombs = []
     def __init__(self, pos, delta):
         imageName = '/bomb.png'
         self.image = gfw_image.load(RES_DIR + imageName)
@@ -11,7 +11,7 @@ class Ball:
         self.delta = delta
         self.radius = self.image.h // 2
     def draw(self):
-        self.image.draw(*self.pos)
+        self.image.draw(*self.pos,50,50)
     def update(self):
         x,y = self.pos
         count=0
@@ -20,4 +20,4 @@ class Ball:
         count = count +delay
 
         if count == 1:
-            Ball.balls.remove(self)
+            Bomb.bombs.remove(self)
